717ff6406a3f3adbcef6c5f65d57083cc9dce55e
Hugo <hugo@zuperito.xyz>
2025-09-20 18:58:50 +0200

    bye bye nix-gaming

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -15,12 +15,10 @@
         "flakes"
       ];
       substituters = [ 
-        "https://nix-gaming.cachix.org" 
         "https://nix-community.cachix.org" 
         "https://cache.nixos.org"
       ];
       trusted-public-keys = [
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -6,12 +6,6 @@
 }:
 {
   home.packages = with pkgs; [
-    ## Utils
-    # gamemode
-    # gamescope
-    # winetricks
-    # inputs.nix-gaming.packages.${pkgs.system}.wine-ge
-
     ## Minecraft
     prismlauncher
 
