a7214f91de6e7beb9dba8b902e3f0c48c1df2876
Hugo <hugo@zuperito.xyz>
2025-10-03 20:53:29 +0200

    home: add .local/bin to PATH

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -23,6 +23,9 @@
       home.homeDirectory = "/home/${username}";
       home.stateVersion = "25.05";
       programs.home-manager.enable = true;
+			home.sessionPath = [
+				"$HOME/.local/bin"
+			];
     };
   };
 
