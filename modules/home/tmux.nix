ff1803f576c3eb7fda8bcdae7d96f0f682fe0e3a
Hugo <hugo@zuperito.xyz>
2025-10-06 12:15:41 +0200

    tmux: new config

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -3,15 +3,21 @@
   programs.tmux = {
     enable = true;
     shell = "${pkgs.zsh}/bin/zsh";
-    terminal = "xterm-kitty";
     historyLimit = 500000;
     extraConfig = ''
-      # Change prefix to C-o
-      unbind C-b
-      set-option -g prefix C-o
-      bind-key C-o send-prefix
-
-      setw -g mode-keys vi
+      set -g prefix C-o
+			set -sg escape-time 10
+      set -g base-index 1
+			set -g renumber-windows on
+			set -g mode-keys vi
+			set -g status-position top
+			set -g status-justify absolute-center
+			set -g status-style "bg=default"
+			set -g window-status-current-style "fg=blue bg=default bold"
+			set -g status-right ""
+			set -g status-left "#S"
+			set -g focus-events on
+			set -g default-terminal "tmux-256color"
 
 			# Copy mode
 			bind-key -T copy-mode-vi 'y' send -X copy-selection
@@ -19,25 +25,10 @@
 			bind-key -T copy-mode-vi 'C-v' send -X rectangle-toggle
 
 
-      # Style points
-      set -g status-style 'bg=#333333 fg=#5eacd3'
-
-      set -g base-index 1
-
-
-      # vim-like pane switching (colemak)
-      bind -r ^ last-window
-      bind -r n select-pane -L
-      bind -r o select-pane -R
-      bind -r e select-pane -D
-      bind -r i select-pane -U
-
-
       # TODO: tmux-sessionasizer
       # bind-key -r f run-shell "tmux nnew ~/.local/bin/tmux-sessionasizer"
   
       # Useful keybindings
-      #bind-key -r l send-keys "tmux neww lazygit" C-m
       bind-key -r l new-window -n lazygit lazygit
     '';
   };
