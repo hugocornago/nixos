685dec49abb77e00ed007feb0f9253fd5dd9328c
Hugo <hugo@zuperito.xyz>
2025-10-16 17:20:57 +0200

    flake: use detsys nix instead

--- flake.nix
+++ flake.nix
@@ -3,68 +3,73 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    determinate = {
+      url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     hypr-contrib.url = "github:hyprwm/contrib";
     hyprpicker.url = "github:hyprwm/hyprpicker";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     hyprmag.url = "github:SIMULATAN/hyprmag";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
     nixcord.url = "github:kaylorben/nixcord";
-		sops-nix = {
-			url = "github:Mic92/sops-nix";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     hyprland = {
       type = "git";
       url = "https://github.com/hyprwm/Hyprland";
       submodules = true;
     };
-		lanzaboote = {
-			url = "github:nix-community/lanzaboote/v0.4.2";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    { nixpkgs, self, sops-nix, ... }@inputs:
-    let
-      username = "cornago";
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
+  outputs = {
+    nixpkgs,
+    self,
+    sops-nix,
+    determinate,
+    ...
+  } @ inputs: let
+    username = "cornago";
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem {
         inherit system;
-        config.allowUnfree = true;
+        modules = [
+          determinate.nixosModules.default
+          ./hosts/desktop
+        ];
+        specialArgs = {
+          host = "desktop";
+          inherit self inputs username;
+        };
       };
-      lib = nixpkgs.lib;
-    in
-    {
-      nixosConfigurations = {
-        desktop = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [ ./hosts/desktop ];
-          specialArgs = {
-            host = "desktop";
-            inherit self inputs username;
-          };
+      laptop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [./hosts/laptop sops-nix.nixosModules.sops];
+        specialArgs = {
+          host = "laptop";
+          inherit self inputs username;
         };
-        laptop = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [ ./hosts/laptop sops-nix.nixosModules.sops ];
-          specialArgs = {
-            host = "laptop";
-            inherit self inputs username;
-          };
-        };
-        vm = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [ ./hosts/vm ];
-          specialArgs = {
-            host = "vm";
-            inherit self inputs username;
-          };
+      };
+      vm = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [./hosts/vm];
+        specialArgs = {
+          host = "vm";
+          inherit self inputs username;
         };
       };
     };
+  };
 }
