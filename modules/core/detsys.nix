c2b6a3e163fe942a90a4688eed8326f42a13d6fc
Hugo <hugo@zuperito.xyz>
2025-10-19 22:49:35 +0200

    detsys: update cache

--- modules/core/detsys.nix
+++ modules/core/detsys.nix
@@ -1,7 +1,7 @@
 {...}: {
   nix.settings = {
-    substituters = ["https://nix-community.cachix.org" "https://cache.cornago.net"];
-    trusted-substituters = ["https://nix-community.cachix.org" "https://cache.cornago.net"];
+    substituters = ["https://nix-community.cachix.org" "https://cache.cornago.net/cache"];
+    trusted-substituters = ["https://nix-community.cachix.org" "https://cache.cornago.net/cache"];
     trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "cache:Zmntlu1k2L4u4wbdF4FLIfjsjs4/QkVYch2Tt7/ZGQ8="];
     trusted-users = [
       "@wheel"
