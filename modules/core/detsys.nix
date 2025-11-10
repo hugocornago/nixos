6be7f39c2f3e71abdfe92f0af1c798a4f4e520e1
Hugo <hugo@zuperito.xyz>
2025-11-10 17:22:27 +0100

    detsys: remove cornago cache

--- modules/core/detsys.nix
+++ modules/core/detsys.nix
@@ -1,10 +1,12 @@
 {...}: {
   nix.settings = {
-    substituters = ["https://nix-community.cachix.org" "https://cache.cornago.net/cache"];
-    trusted-substituters = ["https://nix-community.cachix.org" "https://cache.cornago.net/cache"];
-    trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "cache:Zmntlu1k2L4u4wbdF4FLIfjsjs4/QkVYch2Tt7/ZGQ8="];
+    substituters = ["https://nix-community.cachix.org"];
+    trusted-substituters = ["https://nix-community.cachix.org"];
+    trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "cache.cornago.net:Zmntlu1k2L4u4wbdF4FLIfjsjs4/QkVYch2Tt7/ZGQ8="];
     trusted-users = [
       "@wheel"
     ];
   };
+
+	nixpkgs.config.segger-jlink.acceptLicense = true;
 }
