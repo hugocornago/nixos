d1992b8df731f6d1b99453dd5f09447928f05b26
Hugo <hugo@zuperito.xyz>
2025-12-21 20:04:30 +0100

    remove: cornago.net cache

--- modules/core/detsys.nix
+++ modules/core/detsys.nix
@@ -2,7 +2,7 @@
   nix.settings = {
     substituters = ["https://nix-community.cachix.org"];
     trusted-substituters = ["https://nix-community.cachix.org"];
-    trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "cache.cornago.net:Zmntlu1k2L4u4wbdF4FLIfjsjs4/QkVYch2Tt7/ZGQ8="];
+    trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
     trusted-users = [
       "@wheel"
     ];
