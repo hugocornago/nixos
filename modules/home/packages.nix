4bbb01029a9cf2a7c9e4e041c0bf148abe6f497d
Hugo <hugo@zuperito.xyz>
2026-03-20 08:43:29 +0100

    add: comma

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -13,6 +13,7 @@ in {
     bitwise # cli tool for bit / hex manipulation
     bottles # wine runner
     caligula # User-friendly, lightweight TUI for disk imaging
+		comma
     #dconf-editor
     eza # ls replacement
     entr # perform action when file change
