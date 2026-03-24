550be2486ce38b70a89967b7308879a74ad82691
Hugo <hugo@zuperito.xyz>
2026-03-24 11:55:43 +0100

    bump nixpkgs from 25.05 to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "FrostPhoenix's nixos configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     determinate = {
       url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
