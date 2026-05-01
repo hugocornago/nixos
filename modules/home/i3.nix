576542fa4fe7f2dc5a2208913a8b9304f508058e
Hugo <hugo@zuperito.xyz>
2026-05-01 14:26:58 +0200

    fixup! add exiled-exchange-2

--- modules/home/i3.nix
+++ modules/home/i3.nix
@@ -5,6 +5,14 @@
   config,
   ...
 }: {
+	services.picom = {
+		enable = true;
+		backend = "xrender";
+		opacityRules = [ 
+			"90:class_g = 'exiled-exchange-2'" 
+		];	
+	};
+
   xsession.windowManager.i3 = {
     enable = true;
     config = {
@@ -16,13 +24,16 @@
       };
 
       startup = [
+        {command = "${pkgs.dunst}/bin/dunst";}
         {command = "steam";}
+        {command = "exec ~/.local/bin/vesktop-hw";}
+        {command = "${pkgs.exiled-exchange-2}/bin/exiled-exchange-2";}
       ];
 
       assigns = {
-        "0: discord" = [{class = "^vesktop$";}];
-        "1: steam" = [{class = "^steam$";}];
-        "2: web" = [{class = "^zen-browser$";}];
+        "0" = [{class = "^vesktop$";}];
+        "1" = [{class = "^steam$";}];
+        "2" = [{class = "^zen-beta$";}];
       };
 
       keybindings = let
@@ -31,10 +42,14 @@
       in
         lib.mkOptionDefault {
           # "${modifier}+Return" = "exec i3-sensible-terminal";
+					"${modifier}+Shift+q" = "exec i3-msg exit";
           "${modifier}+w" = "kill";
           "${modifier}+r" = "exec ${pkgs.dmenu}/bin/dmenu_run";
           "${modifier}+b" = "exec ${zen-browser}/bin/zen-beta";
-          "${modifier}+d" = "exec ~/.local/bin/vesktop-hw";
+          "${modifier}+d" = "[class=\"vesktop\"] focus";
+					"F6" = "exec ${pkgs.volume}/bin/volume down 5";
+					"F7" = "exec ${pkgs.volume}/bin/volume up 5";
+					"F12" = "exec ${pkgs.volume}/bin/volume sink";
         };
     };
   };
