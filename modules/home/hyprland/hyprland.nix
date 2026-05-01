ca9892cdbc3c7a406a249586401a3bce3e1226c1
Hugo <hugo@zuperito.xyz>
2026-05-01 12:03:54 +0200

    don't autostart hyprland

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -12,9 +12,9 @@
     wayland
     direnv
   ];
-  systemd.user.targets.hyprland-session.Unit.Wants = [
-    "xdg-desktop-autostart.target"
-  ];
+  # systemd.user.targets.hyprland-session.Unit.Wants = [
+  #   "xdg-desktop-autostart.target"
+  # ];
   wayland.windowManager.hyprland = {
     enable = true;
     package = pkgs.unstable.hyprland;
