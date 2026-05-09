569b0edfda4d7372533511ca275eb4ab1a0de683
Hugo <hugo@zuperito.xyz>
2026-05-09 09:42:20 +0200

    disable command-not-found

--- flake.nix
+++ flake.nix
@@ -69,6 +69,7 @@
           {
             nixpkgs.overlays = [overlay-unstable];
             programs.nix-index-database.comma.enable = true;
+            programs.command-not-found.enable = false;
           }
 
           ./hosts/desktop
