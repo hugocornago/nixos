144b21cda8df6e7c103b3c06e800f0408c41ed1f
Hugo <hugo@zuperito.xyz>
2026-06-21 00:12:09 +0200

    alejandra

--- pkgs/share/default.nix
+++ pkgs/share/default.nix
@@ -1,19 +1,15 @@
-{
-  pkgs
-}:
-let
+{pkgs}: let
   script-name = "share.sh";
-  script = (pkgs.writeScriptBin "${script-name}" (builtins.readFile ./share.sh)).overrideAttrs(old: {
+  script = (pkgs.writeScriptBin "${script-name}" (builtins.readFile ./share.sh)).overrideAttrs (old: {
     buildCommand = "${old.buildCommand}\n patchShebangs $out";
   });
 in
-with pkgs;
-symlinkJoin
-rec {
-  name = "share.sh";
-  description = "Utility to upload video clips to my homeserver.";
-  buildInputs = [ makeWrapper ffmpeg file gawk rsync ];
-  paths = [script] ++ buildInputs;
-  postBuild = "wrapProgram $out/bin/${script-name} --prefix PATH : $out/bin";
-}
-
+  with pkgs;
+    symlinkJoin
+    rec {
+      name = "share.sh";
+      description = "Utility to upload video clips to my homeserver.";
+      buildInputs = [makeWrapper ffmpeg file gawk rsync];
+      paths = [script] ++ buildInputs;
+      postBuild = "wrapProgram $out/bin/${script-name} --prefix PATH : $out/bin";
+    }
